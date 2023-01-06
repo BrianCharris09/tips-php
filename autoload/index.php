64d8858afc75ef75eee2c7519c9aaa144b62e0ac
Brian <brayandejesuscharris@gmail.com>
2023-01-05 22:59:29 -0500

    Finalizado las pruebas de autoload

--- autoload/index.php
+++ autoload/index.php
@@ -1,16 +1,17 @@
 <?php
 
-use \Controllers\CourseController;
-use \Models\Course;
+use Controllers\CourseController;
+use Models\Course;
 
-spl_autoload_register(
-    function ($clase) {
-        $file = str_replace('\\','/',$clase).'.php';
-        if (!file_exists($file)) {
-            # code...
-        }
+spl_autoload_register(function ($clase) {
+    $file = str_replace('\\','/',$clase).'.php';
+    if (file_exists($file)) {
         require_once $file;
     }
-);
+});
 
 $curso = new Course;
+$curso->saludar();
+
+$curso2 = new CourseController;
+$curso2->saludar();
